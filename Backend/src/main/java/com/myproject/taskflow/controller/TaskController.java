7943d83381017eabb5380628559db11cfb654684
Ibrahim Addou <ibrahimaddou2@gmail.com>
2025-06-22 16:32:16 +0200

    feat : add task controller

---
+++ Backend/src/main/java/com/myproject/taskflow/controller/TaskController.java
@@ -0,0 +1,20 @@
+package com.myproject.taskflow.controller;
+
+import com.myproject.taskflow.entities.Task;
+import com.myproject.taskflow.services.TaskService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+    
+}
